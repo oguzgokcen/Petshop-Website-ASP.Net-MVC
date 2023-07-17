35900c37b4f761817c63fddfd1cc4d8e4084b96a
Oğuz Gökçen <oguzgok21@gmail.com>
2023-07-17 18:51:05 +0300

    Base project with Product page and Detail Page

---
+++ Proje/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje.Models;
+using TurkcellModel.Models;
+
+namespace Proje.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Urunler()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Detay(int id)
+        {
+            var product = _context.Products.Find(id);
+            return View(product);
+        }
+
+    }
+}
---
+++ Proje/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proje.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description{ get; set; }
+        public string imageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set;}
+        public string Type { get; set; }
+    }
+
+}
